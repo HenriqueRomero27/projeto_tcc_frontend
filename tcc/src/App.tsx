12667a1faf96529ebe9771a51965b2efe79758a5
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-03-25 17:49:26 +0000

    footer

--- tcc/src/App.tsx
+++ tcc/src/App.tsx
@@ -1,11 +1,13 @@
 import './root.css'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div className="page">
       <Header />
+      <Footer />
     </div>
   )
 }
--- tcc/src/components/Footer.tsx
+++ tcc/src/components/Footer.tsx
@@ -1,16 +1,31 @@
 import './Footer.css'
+import Logo from "../assets/LOGO.svg"
 
 function Footer() {
     return (
         <div className="footer">
-            <p>Deseja adotar um animalzinho?</p>
+            <h2>Deseja adotar um animalzinho?</h2>
             <button type='button' id='adoptAnimal'>Adotar</button>
             <div className="line"></div>
             <div className="content">
-                
+                <img src={Logo} id="logo" />
+                <div className="links">
+                    <div className="link">
+                        <p className="title">Casas de adoção</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing </p>
+                </div>
+                <div className="link">
+                    <p className="title">Contatos</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing </p>
+                </div>
+                <div className="link">
+                    <p className="title">Redes Sociais</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing </p>
+                </div>
+                </div>
             </div>
             <div className="line"></div>
-
+            <p id="copy">Todos os direitos foram reservados &copy;</p>
         </div>
     )
 }
