c5ff9571f175691855fe3bf8208484b03daf609b
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-03-28 00:36:13 +0000

    add others products

--- tcc/src/pages/Marketplace.tsx
+++ tcc/src/pages/Marketplace.tsx
@@ -5,6 +5,9 @@ import Banner from "../assets/bannerMarketplace.svg"
 import Toys from "../assets/toys.svg"
 import Products from "../assets/products.svg"
 import Hidradante from "../assets/hidradante.svg"
+import Perfume from "../assets/perfume.svg"
+import Protetor from "../assets/protetor.svg"
+import KitPastaEscova from "../assets/kitEscova.svg"
 
 function Marketplace() {
     return (
@@ -26,10 +29,10 @@ function Marketplace() {
 
                 <h2>COSMÉTICOS PARA PETS</h2>
                 <div className="products">
-                    <ProductCard name={"Hidradante de Patinhas"} picture={Hidradante} price={48.98} description={"200ML, PARA DEIXAR AS PATAS DOS SEUS PETS HIDRATADAS!"} />
-                    <ProductCard name={"Hidradante de Patinhas"} picture={Hidradante} price={48.98} description={"200ML, PARA DEIXAR AS PATAS DOS SEUS PETS HIDRATADAS!"} />
-                    <ProductCard name={"Hidradante de Patinhas"} picture={Hidradante} price={48.98} description={"200ML, PARA DEIXAR AS PATAS DOS SEUS PETS HIDRATADAS!"} />
-                    <ProductCard name={"Hidradante de Patinhas"} picture={Hidradante} price={48.98} description={"200ML, PARA DEIXAR AS PATAS DOS SEUS PETS HIDRATADAS!"} />
+                    <ProductCard name={"Hidradante de Patinhas"} picture={Hidradante} price={49.90} description={"200ML, PARA DEIXAR AS PATAS DOS SEUS PETS HIDRATADAS!"} />
+                    <ProductCard name={"Perfume de Pets"} picture={Perfume} price={55.50} description={"150ML, PARA DEIXAR SEUS PETS PERFUMADOS E CHEIROSOS!"} />
+                    <ProductCard name={"Protetor Solar para Pets"} picture={Protetor} price={89.90} description={"80ML, A MELHOR ESCOLHA PARA MANTER SEUS PETS PROTEGIDOS!"} />
+                    <ProductCard name={"Kit - Pasta de Dente e Escova"} picture={KitPastaEscova} price={35.60} description={"A MELHOR OPÇÃO PARA A SAÚDE BUCAL DO SEU PET!"} />
                 </div>
             </div>
         </>
