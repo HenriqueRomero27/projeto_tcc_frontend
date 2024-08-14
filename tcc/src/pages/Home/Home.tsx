5671f92f78003dd08bdc37a56780bc7b35125899
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-08-14 17:53:23 +0000

    fix some errors

--- tcc/src/pages/Home/Home.tsx
+++ tcc/src/pages/Home/Home.tsx
@@ -1,9 +1,5 @@
-import Carousel from "../../components/Carousel/Carousel"
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
-import foto1 from "../../assets/foto1.jpg"
-import foto2 from "../../assets/foto2.jpg"
-import foto3 from "../../assets/foto3.jpg"
 
 function Home() {
     return(
